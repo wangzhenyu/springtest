282a4247e67a2656dbfcb3fadf02cae165800635
wangzhenyu <a113295976@gmail.com>
2014-05-18 17:04:48 +0800

    test -add

--- src/main/java/com/wang/web/action/User.java
+++ src/main/java/com/wang/web/action/User.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class User {
 	@RequestMapping("/test")
 	public String test(){
+		System.out.println("test");
 		return "test";
 	}
 }
